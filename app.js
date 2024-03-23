3b4f37f87965c2b92fc18f1715a654bc94165f22
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-03-23 17:14:35 +0530

    Enabled express error handling middleware in app.js with errorHandler.js middleware function as argument.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 // Middleware to parse json request body.
 app.use(express.json());
+
+// Middleware to handle Errors.
+app.use(errorHandler);
 
 module.exports = app;
