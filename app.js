dee51bcb8d0abeb0df137a89291e27c66a099975
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-03-24 19:06:22 +0530

    Rerouted all API request starting with '/api' route to userRoutes.

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
+const userRoutes = require("./controllers/user.controller");
+
 // Middleware to parse json request body.
 app.use(express.json());
+
+// Reroute all API request starting with "/api" route to userRoutes.
+app.use("/api", userRoutes);
 
 // Middleware to handle Errors.
 app.use(errorHandler);
