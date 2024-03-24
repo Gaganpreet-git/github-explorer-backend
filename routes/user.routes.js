cf149096235a4b446ce079bf62465efd9821116f
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-03-24 19:02:10 +0530

    Mapped routes to thier corresponding controller functions.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,22 +1,24 @@
 const express = require("express");
+const userController = require("../controllers/user.controller");
 const router = express.Router();
 
 // Route to save a new user.
-router.get("/save-user/:username");
+router.get("/save-user/:username", userController.saveUser);
 
 // Route to find mutual followers.
-router.get("/find-mutual-followers/:username");
+router.get("/find-mutual-followers/:username"),
+  userController.getMutualFollowers;
 
 // Route to search users.
-router.get("/search-users");
+router.get("/search-users", userController.searchUsers);
 
 // Route to delete a user.
-router.delete("/delete-user/:username");
+router.delete("/delete-user/:username", userController.deleteUser);
 
 // Route to update a user.
-router.patch("/update-user/:username");
+router.patch("/update-user/:username", userController.updateUser);
 
 // Route to get all users.
-router.get("/list-users");
+router.get("/list-users", userController.getUsers);
 
 module.exports = router;
